5f57e09cdc3e31cd2a2d76bfb913795b37f1e389
wmmnola <wmmnola@gmail.com>
2017-02-01 21:20:12 -0600

    Began work on the commuter class

--- station.js
+++ station.js
@@ -5,6 +5,8 @@ var Station = function(x, y) {
   this.connections = [];
   this.color = color(0, 0, 0);
   this.selected = false;
+  this.population = [];
+  this.populationCap = random(1, 5);
   this.show = function() {
     if (this.selected) this.color = color(204, 204, 0);
     else this.color = color(0, 0, 0);
@@ -12,6 +14,15 @@ var Station = function(x, y) {
     fill(this.color);
     ellipse(this.x, this.y, this.r, this.r);
 
+  }
+  this.update = function() {
+    if (this.population.length < this.populationCap) {
+      pop = new Commuter(this, stations[random(stations.length - 1)]);
+      this.population.push()
+    }
+    for (var i = 0; i < population.length; i++) {
+      population[i].update();
+    }
   }
   this.turnRed = function() {
     fill(255, 0, 0);
