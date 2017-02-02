d683b6436fb9cd5de361b59e338c96a1d2718f5b
wmmnola <wmmnola@gmail.com>
2017-02-01 21:20:31 -0600

    added commuter class

---
+++ commuter.js
@@ -0,0 +1,13 @@
+var Commuter = function(start, end) {
+  this.startNode = start;
+  this.endNode = node;
+  this.currentNode = this.startNode;
+  this.dead = false;
+  this.update = function() {
+    if (this.currentNode == this.endNode) {
+      this.dead = true;
+    } else if {
+
+    }
+  }
+}
